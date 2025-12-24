fad6894551cdbf67ad9cf976a097fd1448e7f880
Will Smith <willtoncel@gmail.com>
2025-12-23 19:51:30 -0500

    Se agregó la funcionalidad al service de registrar equipos

--- GestionPrestamoEquipos.Application/Services/EquipoService/RegistraEquipoService.cs
+++ GestionPrestamoEquipos.Application/Services/EquipoService/RegistraEquipoService.cs
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 using GestionPrestamoEquipos.Application.Interfaces;
 using GestionPrestamoEquipos.Application.DToS;
 using GestionPrestamoEquipos.Application.DToS.EquipoDToS;
+using GestionPrestamoEquipos.Domain.Entities;
 
 namespace GestionPrestamoEquipos.Application.Services.EquipoService
 {
     internal class RegistraEquipoService
     {
         private readonly IEquipoRepository _equipoRepository;
+        private readonly ITipoEquipoRepository _tipoEquipoRepository;
+        private readonly IEstadoEquipoRepository _estadoEquipoRepository;
 
         public RegistraEquipoService(IEquipoRepository equipoRepository)
         {
@@ -23,14 +26,43 @@ namespace GestionPrestamoEquipos.Application.Services.EquipoService
             if (
                 string.IsNullOrWhiteSpace(equipoDTO.descripcion) ||
                 string.IsNullOrWhiteSpace(equipoDTO.nombreEquipo) ||
-                string.IsNullOrWhiteSpace(equipoDTO.serial) ||
-                equipoDTO.idEstado == 0 ||
-                equipoDTO.idTipo == 0
+                string.IsNullOrWhiteSpace(equipoDTO.serial)
                 )
             {
                 throw new Exception("Debe actualizar almenos un campo");
             }
 
+            if ( equipoDTO.idEstado == 0)
+            {
+                throw new Exception("Seleccione el estado en el que se encuentra el equipo");
+            }
+            if (equipoDTO.idTipo == 0)
+            {
+                throw new Exception("Seleccione un tipo de equipo");
+            }
+
+            TipoEquipo tipoEquipoBuscado = _tipoEquipoRepository.TipoEspecifico(equipoDTO.idTipo);
+
+            if (tipoEquipoBuscado == null)
+            {
+                throw new Exception("Tipo equipo invalido");
+            }
+
+            EstadoEquipo estadoEquipoBuscado = _estadoEquipoRepository.EstadoEquipoEspecifico(equipoDTO.idEstado);
+
+            if (estadoEquipoBuscado == null)
+            {
+                throw new Exception("Estado equipo invalido");
+            }
+
+            Equipo equipo = new Equipo();
+            equipo.cambiardescripcion(equipoDTO.descripcion);
+            equipo.cambiarNombreEquipo(equipoDTO.nombreEquipo);
+            equipo.cambiarEstadoEquipo(estadoEquipoBuscado);
+            equipo.cambiarSerial(equipoDTO.serial);
+            equipo.cambiarTipoEquipo(tipoEquipoBuscado);
+
+            _equipoRepository.Agregar(equipo);
 
 
         }
