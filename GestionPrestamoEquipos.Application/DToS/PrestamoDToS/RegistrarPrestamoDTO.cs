eb5df56d4d365d39dc2ca497cae258bd57b9e458
Will Smith <willtoncel@gmail.com>
2025-12-27 11:41:57 -0500

    Se agregó al service la funcionalidad de agregar prestamo

---
+++ GestionPrestamoEquipos.Application/DToS/PrestamoDToS/RegistrarPrestamoDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPrestamoEquipos.Application.DToS.PrestamoDToS
+{
+    public class RegistrarPrestamoDTO
+    {
+        public int idEmpleado { get; set; }
+        public int idEquipo { get; set; }
+
+        public DateTime fechaPrestamo { get; set; }
+
+        public DateTime fechaEstimadaDevolucion { get; set; }
+
+        public string observaciones { get; set; }
+    }
+}
---
+++ GestionPrestamoEquipos.Application/Services/PrestamoService/RegistrarPrestamoService.cs
@@ -0,0 +1,69 @@
+﻿using GestionPrestamoEquipos.Application.DToS.PrestamoDToS;
+using GestionPrestamoEquipos.Application.Interfaces;
+using GestionPrestamoEquipos.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPrestamoEquipos.Application.Services.PrestamoService
+{
+    public class RegistrarPrestamoService
+    {
+        private readonly IPrestamoRepository _prestamoRepository;
+        private readonly IEmpleadoRepository _empleadoRepository;
+        private readonly IEquipoRepository _equipoRepository;
+
+        public RegistrarPrestamoService(IPrestamoRepository prestamoRepository, IEmpleadoRepository empleadoRepository, IEquipoRepository equipoRepository)
+        {
+            this._prestamoRepository = prestamoRepository;
+            _empleadoRepository = empleadoRepository;
+            _equipoRepository = equipoRepository;
+        }
+
+        public void Ejecutar(RegistrarPrestamoDTO prestamoDTO)
+        {
+            if (string.IsNullOrWhiteSpace(prestamoDTO.observaciones))
+            {
+                throw new Exception("El campo de observaciones no puede estar vacio");
+            }
+
+            Empleado empleadoBUscado = _empleadoRepository.EmpleadoEspecifico(prestamoDTO.idEmpleado);
+            Equipo equipoBuscado = _equipoRepository.EquipoEspecifico(prestamoDTO.idEquipo);
+
+            if (equipoBuscado == null)
+            {
+                throw new Exception("Seleccione un equipo valido");
+            }
+            if (empleadoBUscado == null)
+            {
+                throw new Exception("Seleccione un empleado valido");
+            }
+
+            
+
+            if (DateTime.Compare(prestamoDTO.fechaPrestamo, prestamoDTO.fechaEstimadaDevolucion) == 0)
+            {
+                throw new Exception("La fecha de prestamo no puede ser igual a la de devolucion");
+            }
+            if (DateTime.Compare(prestamoDTO.fechaPrestamo, prestamoDTO.fechaEstimadaDevolucion) > 0)
+            {
+                throw new Exception("La fecha de devolucion no puede ser menor a la de prestamo");
+            }
+
+            Prestamo prestamo = new Prestamo();
+            prestamo.cambiarFechaPrestamo(prestamoDTO.fechaPrestamo);
+            prestamo.cambiarFechaDevolucion(prestamoDTO.fechaEstimadaDevolucion);
+            prestamo.cambiarEquipo(equipoBuscado);
+            prestamo.cambiarEmpleado(empleadoBUscado);
+            prestamo.cambiarObservaciones(prestamoDTO.observaciones);
+
+            _prestamoRepository.Agregar(prestamo);
+
+
+        }
+
+
+    }
+}
